c303a1feb12b99ac0fbd89e8b2cbc8b3cb39a0ea
Ajver <zawlockidominik@gmail.com>
2018-10-03 17:54:04 +0200

    Added fPoint class to Camera

--- src/Other/Camera.java
+++ src/Other/Camera.java
@@ -15,14 +15,29 @@ public class Camera {
 	private int pmx, pmy;
     private boolean isDragging = false;
 
-	private Creature c;
-	
-	
+    private Creature c;
+
+    private class fPoint {
+    	float x, y;
+    	public fPoint(float x, float y) {
+    		set(x, y);
+		}
+		public void set(float x, float y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	private fPoint p;
+
+
 	public Camera(Handler handler) {
-		this.c = handler.creatures.get(0);
-		
+		Creature c = handler.creatures.get(0);
+
 		this.x = c.getX() - MainClass.WW / 2.0f + Handler.cellW / 2.0f;
 		this.y = c.getY() - MainClass.WH / 2.0f + Handler.cellH / 2.0f;
+
+		p = new fPoint(x, y);
 	}
 	
 	public void update(float et) {
